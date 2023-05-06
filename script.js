71c5559b269f72e6883d69454751b94d896b348e
Erol Ayhan <e.erolayhan@gmail.com>
2023-05-06 20:25:57 +0300

    tables added

---
+++ script.js
@@ -0,0 +1,7 @@
+const table1 = document.getElementById("table1");
+
+table1.addEventListener("click", function() {
+  // add your code to execute when the table is clicked
+  console.log("Table 1 clicked");
+
+});
